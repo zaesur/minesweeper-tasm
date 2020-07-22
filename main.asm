6fb32b2621009fc27e8d353b3db3e2219810007b
fvanbelleghem <fvbe@stylelabs.com>
2020-07-22 12:44:29 +0200

    Implement printBoard

--- main.asm
+++ main.asm
@@ -6,7 +6,7 @@ ASSUME cs:_TEXT,ds:FLAT,es:FLAT,fs:FLAT,gs:FLAT
 INCLUDE "print.inc"
 INCLUDE "cell.inc"
 
-GRID_SIZE EQU 5
+GRID_SIZE EQU 6
 
 CODESEG
 
@@ -14,21 +14,15 @@ proc main
     sti
     cld
     
-    call printCell, offset hiddenCell
-    call printCell, offset revealedCell
-    call printCell, offset hiddenMine
-    call printCell, offset revealedMine
-    
+    call printBoard, GRID_SIZE, offset board
+
     mov 	eax, 4c00h      ; AH = 4Ch - Exit To DOS
     int 	21h             ; DOS INT 21h
 endp main
 
 STACK 100h
 
-DATASEG
-    hiddenCell Cell <0, 0, 5>
-    revealedCell Cell <0, 1, 5>
-    hiddenMine Cell <1, 0, 5>
-    revealedMine Cell <1, 1, 5>
+UDATASEG
+    board Cell GRID_SIZE*GRID_SIZE dup(<>)
 
 end main
