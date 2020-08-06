1f1ac46323175948ea16da92d100c2dbd4a7e6b5
fvanbelleghem <fvbe@stylelabs.com>
2020-08-06 20:52:06 +0200

    implement draw in video mode 13h

--- main.asm
+++ main.asm
@@ -3,26 +3,26 @@ P386
 MODEL FLAT, C
 ASSUME cs:_TEXT,ds:FLAT,es:FLAT,fs:FLAT,gs:FLAT
 
-INCLUDE "print.inc"
+INCLUDE "io.inc"
 INCLUDE "cell.inc"
+INCLUDE "mouse.inc"
 
 CODESEG
 
 proc main
     sti
     cld
-    
-    call placeMines, offset board, GRID_SIZE
-    call revealCell, offset board, 0, 0
-    call printBoard, offset board
 
-    mov 	eax, 4c00h      ; AH = 4Ch - Exit To DOS
-    int 	21h             ; DOS INT 21h
+    call setVideoMode, 13h
+    call placeMines, offset board
+    call drawBoard, offset board
+    call waitForSpecificKeystroke, 01Bh
+    call terminateProcess
+
 endp main
 
 STACK 100h
 
 DATASEG
-    board Cell GRID_SIZE*GRID_SIZE dup(<0,0,0>)
-
+    board Cell GRID_SIZE*GRID_SIZE dup(<0,1,0>)
 end main
