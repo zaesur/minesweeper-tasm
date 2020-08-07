9eb003ffb8b38afeb95afa45d327d4607ba6a8bc
fvanbelleghem <fvbe@stylelabs.com>
2020-08-07 15:15:27 +0200

    fix mouseHandler

--- main.asm
+++ main.asm
@@ -9,34 +9,36 @@ INCLUDE "mouse.inc"
 
 CODESEG
 
-proc handler
+call mouseHandler
     local @@row:dword, @@col:dword
     uses eax, ebx, ecx, edx
 
     and bl, 3
     jz @@skip
-    
-    sar cx, 3
+
+    sar cx, 4
     cmp cx, GRID_SIZE
     jge @@skip
-    
+    mov [@@col], ecx
+
     sar dx, 3
     cmp dx, GRID_SIZE
     jge @@skip
+    mov [@@row], edx
 
-    call revealCell, offset board, cx, dx
+    call revealCell, offset board, [@@row], [@@col]
     call drawBoard, offset board
 
 @@skip:
     ret
-endp handler
+endp mouseHandler
 
 proc main
     sti
     cld
 
     call setVideoMode, 13h
-    call mouse_install, offset handler
+    call mouse_install, offset mouseHandler
     mov ax, 01h
     int 33h
     call placeMines, offset board
@@ -50,4 +52,5 @@ STACK 100h
 
 DATASEG
     board Cell GRID_SIZE*GRID_SIZE dup(<0,0,0>)
+
 end main
